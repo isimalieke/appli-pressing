363bb583fb232094e34816f579bf8b6557b22672
Isima Lieke <isima.lieke@gmail.com>
2026-07-18 06:50:52 +0000

    Corrige CORS : autorise PUT (bloquait la sélection des soins)

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@
 
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET,POST,PATCH,DELETE,OPTIONS',
+  'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type',
 }
 
